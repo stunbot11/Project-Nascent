d6ab6175936444ce8bf6fb9e4f6c7f17fcd74754
stunbot11 <zachary@borda.us>
2024-06-17 14:48:35 -0700

    added collsions, camera following, and slime

--- Scripts/player.cs
+++ Scripts/player.cs
@@ -3,7 +3,7 @@ using Godot;
 public partial class player : CharacterBody2D
 {
     [Export]
-    public int Speed { get; set; } = 400;
+    public int Speed { get; set; } = 100;
 
     private Vector2 _target;
 
@@ -19,7 +19,7 @@ public partial class player : CharacterBody2D
     {
         Velocity = Position.DirectionTo(_target) * Speed;
         // LookAt(_target);
-        if (Position.DistanceTo(_target) > 10)
+        if (Position.DistanceTo(_target) > 1)
         {
             MoveAndSlide();
         }
