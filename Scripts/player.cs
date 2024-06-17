a9387cf7bbfedf69f81c3df162d2eeca47fd5a53
jessica <jbjabzin@gmail.com>
2024-06-17 13:58:29 -0700

    Right click movement Done

---
+++ Scripts/player.cs
@@ -0,0 +1,27 @@
+using Godot;
+
+public partial class player : CharacterBody2D
+{
+    [Export]
+    public int Speed { get; set; } = 400;
+
+    private Vector2 _target;
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event.IsActionPressed("Move"))
+        {
+            _target = GetGlobalMousePosition();
+        }
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Velocity = Position.DirectionTo(_target) * Speed;
+        // LookAt(_target);
+        if (Position.DistanceTo(_target) > 10)
+        {
+            MoveAndSlide();
+        }
+    }
+}
