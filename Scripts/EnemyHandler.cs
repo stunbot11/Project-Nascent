cf147109dbb27b631d0df5dbfaef3f697a1f9e16
stunbot11 <zachary@borda.us>
2024-06-19 20:43:06 -0700

    added the enemy
    added enemy generation, enemy deletion, and enemy following of a set path

---
+++ Scripts/EnemyHandler.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public partial class Enemy : PathFollow2D
+{
+    private int _healthpoints;
+    private float _speed;
+
+    private gameManager gameManager;
+
+    public override void _Ready()
+    {
+        gameManager = GetNode<gameManager>("/root/Root");
+    }
+
+    public override void _Process(double delta)
+    {
+        ProgressRatio += _speed * (float)delta * 0.05f;
+        if (ProgressRatio == 1) // reached the end of the path
+            gameManager.EndEnemy(this);
+    }
+
+    public void Initialize(Texture2D sprite, int healthpoints, float speed)
+    {
+        GetNode<Sprite2D>("Sprite2D").Texture = sprite;
+        _healthpoints = healthpoints;
+        _speed = speed;
+    }
+
+    private void _OnClicked(Node viewport, InputEvent @event, int shapeIdx)
+    {
+        if (@event is InputEventMouseButton e && e.ButtonIndex == MouseButton.Left && e.Pressed)
+        {
+            _healthpoints--;
+            if (_healthpoints == 0)
+                gameManager.EndEnemy(this);
+        }
+    }
+}
--- Scripts/gameManager.cs
+++ Scripts/gameManager.cs
@@ -1,12 +1,48 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class gameManager : Node2D
 {
+    [Export] private PackedScene enemyScene;
+	[Export] private Timer enemySpawnTimer;
+    
 	private RadialMenu radialMenu;
-	// Called when the node enters the scene tree for the first time.
+    private List<int> waveData = new();
+
+    private int currentWavePosition;
+	private int currentWaveIndex;
+	private int waveCount;
+	private Node enemiesParent;
+
+    // Called when the node enters the scene tree for the first time.
+
+    struct EnemyData //defines the characterisitcs of the enemeies
+	{
+		public Texture2D sprite;
+		public int healthPoints;
+		public int speed;
+	}
+
+	private List<EnemyData> enemyData = new() //finds the png's for the enemies
+	{
+		new EnemyData {sprite = GD.Load<Texture2D>("res://Assets/brackeys_platformer_assets/sprites/slime_green.png"), healthPoints = 5, speed = 3 },
+		new EnemyData {sprite = GD.Load<Texture2D>("res://Assets/brackeys_platformer_assets/sprites/slime_purple.png"), healthPoints = 10, speed = 1 },
+	};
+
+	private List<(int, int)[]> waves = new() //defines what spawns on what wave
+	{
+		new (int, int)[] {(0,2), (1,2)},
+		new (int, int)[] {(0,7), (1,2)},
+		new (int, int)[] {(0,3), (1,3), (0,5)},
+		new (int, int)[] {(1,3), (0, 10)},
+		new (int, int)[] {(0,15), (1,10), (0,5)},
+	};
+
 	public override void _Ready()
 	{
+		enemiesParent = GetNode("Enemy path");
+		StartWave(0);
 		radialMenu = GetNode<RadialMenu>("RadialMenu");
 		radialMenu.Hide();
 	}
@@ -28,4 +64,43 @@ public partial class gameManager : Node2D
 	public override void _Process(double delta)
 	{
 	}
+
+	private void StartWave(int waveIndex)
+	{
+		currentWaveIndex = waveIndex;
+
+		waveData.Clear();
+		foreach ((int type, int count) in waves[waveIndex])
+		{
+			for (int i = 0; i < count; i++)
+			{
+				waveData.Add(type);
+			}
+		}
+		currentWavePosition = 0;
+		waveCount = waveData.Count;
+		enemySpawnTimer.Start();
+	}
+
+	private void OnSpawnEnemy()
+	{
+		if (currentWavePosition >= waveData.Count) return;
+		
+            int enemyType = waveData[currentWavePosition];
+            currentWavePosition++;
+
+			Enemy enemy = enemyScene.Instantiate<Enemy>();
+			enemiesParent.AddChild(enemy);
+			EnemyData d = enemyData[enemyType];
+			enemy.Initialize(d.sprite, d.healthPoints, d.speed);
+        
+	}
+
+    public void EndEnemy(Node enemy)
+    {
+        enemy.QueueFree();
+        waveCount--;
+
+        if (waveCount == 0) enemySpawnTimer.Stop();
+    }
 }
