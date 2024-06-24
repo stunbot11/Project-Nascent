a89e2e578b279de99118fc86aefb786f77a74cdd
stunbot11 <zachary@borda.us>
2024-06-24 16:58:52 -0700

    jay's changes added zooming in and out and switched main scene

--- 3DVersion/Scripts/CursorManager.cs
+++ 3DVersion/Scripts/CursorManager.cs
@@ -3,22 +3,29 @@ using System;
 
 public partial class CursorManager : Node
 {
-	Texture2D defaultCursor;
-	Texture2D grabCursor;
+    Texture2D defaultCursor;
+    Texture2D grabCursor;
+    Texture2D zoomInCursor;
+    Texture2D zoomOutCursor;
 
-	public override void _Ready()
-	{
-		defaultCursor = ResourceLoader.Load<Texture2D>("res://3DVersion/Assets/hand_point.png");
-		grabCursor = ResourceLoader.Load<Texture2D>("res://3DVersion/Assets/hand_closed.png");
+    public override void _Ready()
+    {
+        defaultCursor = ResourceLoader.Load<Texture2D>("res://3DVersion/Assets/hand_point.png");
+        grabCursor = ResourceLoader.Load<Texture2D>("res://3DVersion/Assets/hand_closed.png");
+        zoomInCursor = ResourceLoader.Load<Texture2D>("res://3DVersion/Assets/zoom_in.png");
+        zoomOutCursor = ResourceLoader.Load<Texture2D>("res://3DVersion/Assets/zoom_out.png");
 
-		Input.SetCustomMouseCursor(defaultCursor);
-	}
+        Input.SetCustomMouseCursor(defaultCursor);
+    }
 
     public override void _Input(InputEvent @event)
     {
-        if(@event is InputEventMouseButton e) {
-			if(e.Pressed) Input.SetCustomMouseCursor(grabCursor);
-			else Input.SetCustomMouseCursor(defaultCursor);
-		}
+        if (@event is InputEventMouseButton e)
+        {
+            if (e.ButtonIndex == MouseButton.WheelUp) Input.SetCustomMouseCursor(zoomInCursor);
+            else if (e.ButtonIndex == MouseButton.WheelDown) Input.SetCustomMouseCursor(zoomOutCursor);
+            else if (e.Pressed) Input.SetCustomMouseCursor(grabCursor);
+            else Input.SetCustomMouseCursor(defaultCursor);
+        }
     }
 }
